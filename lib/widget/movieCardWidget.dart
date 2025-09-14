647f57e7c40863d65da30ad29dad4464cb0b125a
adamluqman <adamluqman1611@gmail.com>
2025-09-15 00:48:16 +0800

    create UI for movie card

---
+++ lib/widget/movieCardWidget.dart
@@ -0,0 +1,31 @@
+import 'package:adamcinemaapp/model/movieModel.dart';
+import 'package:flutter/material.dart';
+
+class MovieCard extends StatelessWidget {
+  final Movie movie;
+
+  const MovieCard({super.key, required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+          child: Image.network(
+            movie.movieImage,
+            height: 180,
+            width: 120,
+            fit: BoxFit.cover,
+            ),
+        ),
+        const SizedBox(height: 5),
+        Text(
+          movie.movieTitle,
+          style: const TextStyle(color: Colors.white, fontSize: 14),
+          softWrap: true,
+        )
+      ],
+    );
+  }
+}

e63a04ec6feea5b80e0f012e644609c2785d504b
adamluqman <adamluqman1611@gmail.com>
2025-09-19 21:47:19 +0800

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:adamcinemaapp/screen/homeScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'provider/movieProvider.dart';
@@ -6,6 +7,8 @@ import 'provider/movieProvider.dart';
 
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
    runApp(
     MultiProvider(
       providers: [

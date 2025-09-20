c5b54f2f358a110ec3269e6e462ee783942424b6
adamluqman <adamluqman1611@gmail.com>
2025-09-20 23:09:56 +0800

    add seat select status into seat model

--- lib/model/cinemaSeatModel.dart
+++ lib/model/cinemaSeatModel.dart
@@ -2,11 +2,13 @@ class Seat {
   final String seatId;
   final String seatStatus;
   final DateTime? lockedDuration;
+  final bool isSeatSelected;
 
   Seat({
     required this.seatId, 
     required this.seatStatus,
-    required this.lockedDuration
+    required this.lockedDuration,
+    this.isSeatSelected = false
   });
 
   factory Seat.fromJson(Map<String, dynamic> json, String seatId) {
@@ -22,5 +24,14 @@ class Seat {
       'seatStatus': seatStatus,
       'lockedDuration': lockedDuration?.toIso8601String()
     };
+  }
+
+  Seat seatSelectStatus({bool? isSelected}) {
+    return Seat(
+      seatId: seatId,
+      seatStatus: seatStatus,
+      lockedDuration: lockedDuration,
+      isSeatSelected: isSelected ?? this.isSeatSelected,
+    );
   }
 }
